d339bf95c225054528d25de86b00804903d5155b
Murad H Sarker <sarker24@gmail.com>
2022-06-19 15:00:32 +0200

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Login from "./components/LogIn/Login";
+import Carusal from "./components/Carusal/Carusal";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="container text-center">
-      <p className="bg-primary">My gub application</p>
+      <Header />
+      <Login />
+      <Carusal />
     </div>
   );
 }
---
+++ src/components/Carusal/Carusal.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+const Carusal = () => {
+  return (
+    <div>
+      <h1>This is carrusal</h1>
+      <Button as="input" type="reset" value="Reset" />
+    </div>
+  );
+};
+
+export default Carusal;
