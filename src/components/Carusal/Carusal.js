0baaf60da6e99c432ec9db64d855c01ac5cf68ce
Murad H Sarker <sarker24@gmail.com>
2022-06-29 14:12:43 +0200

    Add carusal

--- src/components/Carusal/Carusal.js
+++ src/components/Carusal/Carusal.js
@@ -1,11 +1,39 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import IMG1 from "../../Images/green3.jpg";
+import IMG2 from "../../Images/green2.jpg";
+import IMG3 from "../../Images/green.jpg";
 const Carusal = () => {
   return (
     <div>
-      <h1>This is carrusal</h1>
-      <Button as="input" type="reset" value="Reset" />
+      <Carousel autoPlay infiniteLoop showThumbs={false}>
+        <div>
+          <img
+            height={350}
+            className=" w-100   object-cover object-center"
+            src={IMG1}
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            height={350}
+            className="h-64 w-100 object-cover object-center"
+            src={IMG2}
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            height={350}
+            className="h-64 w-100 object-cover object-center"
+            src={IMG3}
+            alt=""
+          />
+          {/*  <p className="legend">Legend 3</p> */}
+        </div>
+      </Carousel>
     </div>
   );
 };
